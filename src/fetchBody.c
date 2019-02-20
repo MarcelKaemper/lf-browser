388f6bfbdd380c2b37b991349d96afd7d0e67526
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-20 18:09:01 +0100

    Implemented function to fetch the html body

---
+++ src/fetchBody.c
@@ -0,0 +1,16 @@
+#include "fetchBody.h"
+#include <curl/curl.h>
+
+void fetchBody(char url[256]) {
+    CURL *curl = curl_easy_init();
+    CURLcode result;
+
+    FILE *f = fopen("data","wb");
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, f);
+
+    result = curl_easy_perform(curl);
+
+    curl_easy_cleanup(curl);
+}
---
+++ src/fetchBody.h
@@ -0,0 +1,6 @@
+#ifndef FETCHBODY_H_   /* Include guard */
+#define FETCHBODY_H_
+
+void fetchBody(char url[256]);
+
+#endif // FETCHBODY_H_
