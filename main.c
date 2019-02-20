49265197115817d6b45368c2356e4fe84a4974be
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-20 17:43:21 +0100

    Writing data to file

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
+#include <ncurses.h>
 
 int main(void) {
     CURL *curl = curl_easy_init();
     CURLcode result;
+    FILE *f = fopen("data","wb");
     curl_easy_setopt(curl, CURLOPT_URL, "http://marcelkaemper.deinsh.eu");
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, f);
     result = curl_easy_perform(curl);
     curl_easy_cleanup(curl);
 
