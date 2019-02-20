b37c12f1f2ddfd2f3a0a3d72fb289e72607f466e
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-20 19:18:05 +0100

    Added dynamic window size; Drawing box around play area

--- main.c
+++ main.c
@@ -5,10 +5,27 @@
 #include "src/fetchBody.h"
 
 int main(void) {
+    int h,w;
     char url[250];
 
     initscr();
-    WINDOW *win = newwin(10,10,10,10);
+    getmaxyx(stdscr, h, w);
+    WINDOW *win = newwin(h,w,0,0);
+
+    // Draw box around the window
+    for(int i = 0; i<h; i++){
+        for(int j = 0; j<w; j++){
+            if((i==0) || (i==h-1)){
+                mvwaddch(win,i,j,ACS_BLOCK);
+            }else{
+                mvwaddch(win, i, 0, ACS_BLOCK);
+                mvwaddch(win, i, w-1, ACS_BLOCK);
+            }
+        }
+    }
+
+    refresh();
+    wrefresh(win);
 
     char c = getch();
 
