d4d852bd1913ba60100c1acdd06fc8c474911869
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-20 18:34:32 +0100

    Added curses window

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <curl/curl.h>
 #include <ncurses.h>
 #include "src/fetchBody.h"
 
 int main(void) {
-    char url[250] = "http://marcelkaemper.deinsh.eu";
+    char url[250];
+
+    initscr();
+    WINDOW *win = newwin(10,10,10,10);
+
+    char c = getch();
+
+    endwin();
+
+    if(c == '1'){
+        strcpy(url,"http://marcelkaemper.deinsh.eu");
+    }
+    
     fetchBody(url);
 
     return 0;
