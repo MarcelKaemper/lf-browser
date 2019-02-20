5348514b32b4b0a283fa6b71b8197cfe9f68989b
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-20 18:08:05 +0100

    Excluded url fetching into external function

--- main.c
+++ main.c
@@ -3,15 +3,11 @@
 #include <string.h>
 #include <curl/curl.h>
 #include <ncurses.h>
+#include "src/fetchBody.h"
 
 int main(void) {
-    CURL *curl = curl_easy_init();
-    CURLcode result;
-    FILE *f = fopen("data","wb");
-    curl_easy_setopt(curl, CURLOPT_URL, "http://marcelkaemper.deinsh.eu");
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, f);
-    result = curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
+    char url[250] = "http://marcelkaemper.deinsh.eu";
+    fetchBody(url);
 
     return 0;
 }
